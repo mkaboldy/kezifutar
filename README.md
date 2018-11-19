4b08e8938adc3695061ee9e782259b543a023bcc
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2018-11-19 16:21:09 +0100

    prod/dev config

--- README.md
+++ README.md
@@ -10,13 +10,33 @@ WIP
 install git https://tortoisegit.org/download/
 install nodejs https://nodejs.org/en/download/
 
-### clone, build and run project:
+### clone and build the server:
 
+```
 your-folder> git checkout https://github.com/mkaboldy/kezifutar.git
 your-folder> npm update
+```
+
+### adjust config:
+
+```
+your-folder/config> vi default.json
+```
+
+(or specify own environment **$env:NODE_ENV="yourenv"** and build your own config, **yourenv.json** )
+
+### run the server:
+
+```
 your-folder> npm install -g nodemon
 your-folder> nodemon index.js
+```
+
+### clone and build the Angular client:
+
+```
 your-folder/angular> npm update
 your-folder/angular> ng serve --host <your-ip> --ssl
-https://<your-IP>:<your-port>
+open your browser https://<your-IP>:<your-port>
+```
 
--- index.js
+++ index.js
@@ -8,25 +8,25 @@ const api = require('./api')(router);
 const path = require('path'); 
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const config = require('config');
 
 app.use(cors({ origin: '*' }));
-app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/angular/dist/angular/')); // Provide static directory for frontend
-// app.use('/authentication', authentication);
-// app.use('/blogs', blogs);
+
 app.use('/api',api);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/angular/dist/angular/index.html'));
 });
 
-const privateKey  = fs.readFileSync(__dirname + '/sslcert/privateKey.key', 'utf8');
-const certificate = fs.readFileSync(__dirname + '/sslcert/certificate.crt', 'utf8');
+const privateKey  = fs.readFileSync(config.ssl_key, 'utf8');
+const certificate = fs.readFileSync(config.ssl_cert, 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8080,'192.168.0.14');
-httpsServer.listen(8443,'192.168.0.14');
+httpServer.listen(config.http_port, config.http_host);
+httpsServer.listen(config.https_port, config.https_host);

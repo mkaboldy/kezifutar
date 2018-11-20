0a3c064082bb11d224fd5398c4a230f88a22ddc8
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2018-11-20 08:56:07 +0100

    project cleanup for release

--- config/hsdev5.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    http_host   : "192.168.0.14",
-    http_port   : 8080,
-    https_host  : "192.168.0.14",
-    https_port  : 8443,
-    ssl_cert    : "C:/Users/Miklos/source/repos/kezifutar/sslcert/certificate.crt",
-    ssl_key     : "C:/Users/Miklos/source/repos/kezifutar/sslcert/privateKey.key"
-}

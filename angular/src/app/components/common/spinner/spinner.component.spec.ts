1d0f6bc43bdaf989dd8a31c51d8d8a4d5183ce0b
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2018-11-24 00:18:14 +0100

    minor LINT fix

--- angular/src/app/components/common/spinner/spinner.component.spec.ts
+++ angular/src/app/components/common/spinner/spinner.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SpinnerComponent } from './spinner.component';
+import { CommonSpinnerComponent } from './spinner.component';
 
 describe('SpinnerComponent', () => {
-  let component: SpinnerComponent;
-  let fixture: ComponentFixture<SpinnerComponent>;
+  let component: CommonSpinnerComponent;
+  let fixture: ComponentFixture<CommonSpinnerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpinnerComponent ]
+      declarations: [ CommonSpinnerComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SpinnerComponent);
+    fixture = TestBed.createComponent(CommonSpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

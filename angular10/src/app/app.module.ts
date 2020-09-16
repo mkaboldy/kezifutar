477c68bb4e78445e0933fd814f38e3bf3f9a3fd5
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-09-16 18:08:23 +0200

    components dir structure

--- angular10/src/app/app.module.ts
+++ angular10/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopnavComponent } from './topnav/topnav.component';
-import { FooterComponent } from './footer/footer.component';
-import { DeparturesComponent } from './departures/departures.component';
-import { LocationComponent } from './location/location.component';
-import { SettingsComponent } from './settings/settings.component';
-import { AboutComponent } from './about/about.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DeparturesComponent } from './components/departures/departures.component';
+import { LocationComponent } from './components/location/location.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [

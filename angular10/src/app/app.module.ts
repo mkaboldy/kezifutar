9829420961aa6d1042c9422164ab4cd7e3aa5bc1
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-09-16 13:02:16 +0200

    Static html content now as NG components

--- angular10/src/app/app.module.ts
+++ angular10/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { FooterComponent } from './footer/footer.component';
+import { DeparturesComponent } from './departures/departures.component';
+import { LocationComponent } from './location/location.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    FooterComponent,
+    DeparturesComponent,
+    LocationComponent,
+    SettingsComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
---
+++ angular10/src/app/departures/departures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departures',
+  templateUrl: './departures.component.html',
+  styleUrls: ['./departures.component.scss']
+})
+export class DeparturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

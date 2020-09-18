b160abd6d7f8031c7fdc7f0290cb8582ddaf2960
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-09-18 12:32:35 +0200

    led_matrix2 font added

--- angular10/src/app/app.component.ts
+++ angular10/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    './../assets/fonts/led-matrix2/fonts.css'
+  ]
 })
 export class AppComponent {
   title = 'angular10';

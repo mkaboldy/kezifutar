8fc5df2bcb56f9268781ab080f3398a2167b8ab2
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-09-28 18:24:34 +0200

    Middleware stops-for-location endpoint implemented

--- api/1.0/bkk.js
+++ api/1.0/bkk.js
@@ -1,15 +1,33 @@
+const util = require('util');
+const axios = require('axios');
+const apiRoot = 'https://futar.bkk.hu/api/query/v1/ws/otp/api/where';
+
 module.exports = (router) => {
 
-    router.get('/departuresdemo', (req, res) => {
-        res.json();
-    });
-
-    router.get('/locationsdemo', (req, res) => {
-        res.json();
-    });
-
-    router.get('*', (req, res) => {
-        res.json();
+    router.get('/stops-for-location/:latlon', (req, res) => {
+        const latlon = req.params.latlon.split(',');
+        const endPoint = 'stops-for-location.json';
+        const lat = latlon[0];
+        const lon = latlon[1];
+        const discoveryRadius = 0.004;
+        const latSpan = discoveryRadius;
+        const lonSpan = discoveryRadius;
+        const request = util.format('%s/%s?%s&%s&%s&%s', 
+            apiRoot, 
+            endPoint,
+            util.format('lat=%s',lat),
+            util.format('lon=%s',lon),
+            util.format('latSpan=%s',latSpan),
+            util.format('lonSpan=%s',lonSpan)
+        );
+        axios.get(request).then(
+            (apiResponse) => {
+                res.json(apiResponse.data);
+            },
+            (error) => {
+                console.error(error);
+            }
+        );        
     });
 
     return router;

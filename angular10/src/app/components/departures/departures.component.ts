e48b4d8003a6f985bbe3e432cca903127e8a483b
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-10-10 11:48:31 +0200

    Departures component with multiple boards

--- angular10/src/app/components/departures/departures.component.ts
+++ angular10/src/app/components/departures/departures.component.ts
@@ -9,45 +9,119 @@ import { Station} from '../../interfaces/station';
 })
 export class DeparturesComponent implements OnInit {
 
-  departureBoard: {
-    station: Station,
-    departures: Array<any>,
+  departureBoards: {
+    stations: any
   };
 
   constructor(
     private bkkService: BkkService,
   ) {
-    this.departureBoard = {
-      station: {
-        ID: 'XXX',
-        name: 'Blaha Lujza tér'
-      },
-      departures: [
-        {
-          line: '6',
-          destination: 'Széll Kálmán tér',
-          departure: '',
-          departing: true
+    this.departureBoards = {
+      stations: {
+        BKK_CSF00065: {
+          ID: 'BKK_CSF00065',
+          name: 'Batthyány tér M+H',
+          departures: [
+            {
+              line: '41',
+              lineDescription: 'Kamaraerdei Ifj. Park | Bécsi út / Vörösvári út',
+              vehicleType: 'TRAM',
+              destination: 'Kamaraerdei Ifjúsági Park',
+              departureMins: 0,
+            },
+            {
+              line: 'H5',
+              lineDescription: 'Szentendre | Batthyány tér',
+              vehicleType: 'RAIL',
+              destination: 'Szentendre',
+              departureMins: 3
+            },
+            {
+              line: '990',
+              lineDescription: 'Rákoskeresztúr, városkp. | Normafa',
+              vehicleType: 'BUS',
+              destination: 'Rákoskert',
+              departureMins: 3
+            },
+            {
+              line: '11',
+              lineDescription: 'Nagybányai út | Batthyány tér M H',
+              vehicleType: 'BUS',
+              destination: 'Nagybányai út',
+              departureMins: 3
+            },
+            {
+              line: '109',
+              lineDescription: 'Batthyány tér M H | Óbuda, Bogdáni út',
+              vehicleType: 'BUS',
+              destination: 'Óbuda, Bogdáni út',
+              departureMins: 4
+            },
+            {
+              line: '19',
+              lineDescription: 'Kelenföld vasútállomás M | Bécsi út / Vörösvári út',
+              vehicleType: 'TRAM',
+              destination: 'Bécsi út / Vörösvári út',
+              departureMins: 5
+            },
+            {
+              line: '19',
+              lineDescription: 'Kelenföld vasútállomás M | Bécsi út / Vörösvári út',
+              vehicleType: 'TRAM',
+              destination: 'Kelenföld vasútállomás M',
+              departureMins: 10
+            },
+            {
+              line: '41',
+              lineDescription: 'Kamaraerdei Ifj. Park | Bécsi út / Vörösvári út',
+              vehicleType: 'TRAM',
+              destination: 'Bécsi út / Vörösvári út',
+              departureMins: 14
+            },
+            {
+              line: '41',
+              lineDescription: 'Kamaraerdei Ifj. Park | Bécsi út / Vörösvári út',
+              vehicleType: 'TRAM',
+              destination: 'KOCSISZÍNBE  Budafok kocsiszín',
+              departureMins: 22
+            },
+            {
+              line: '990',
+              lineDescription: 'Rákoskeresztúr, városkp. | Normafa',
+              vehicleType: 'BUS',
+              destination: 'Normafa',
+              departureMins: 24
+            },
+            {
+              line: '109',
+              lineDescription: 'Batthyány tér M H | Óbuda, Bogdáni út',
+              vehicleType: 'BUS',
+              destination: 'Óbuda, Bogdáni út',
+              departureMins: 26
+            }
+          ]
         },
-        {
-          line: '4',
-          destination: 'Újbuda-központ M',
-          departure: '',
-          departing: true
-        },
-        {
-          line: '4',
-          destination: 'Széll Kálmán tér',
-          departure: 5,
-          departing: false
-        },
-        {
-          line: '6',
-          destination: 'Móricz Zsigmond körtér M',
-          departure: 9,
-          departing: false
+        BKK_CSF00166: {
+          ID: 'BKK_CSF00166',
+          name: 'Budagyöngye',
+          departures: [
+            {
+              line: '56A',
+              lineDescription: 'Móricz Zs. körtér M | Hűvösvölgy',
+              vehicleType: 'TRAM',
+              destination: 'Hűvösvölgy',
+              departureMins: 4
+            },
+            {
+              line: '61',
+              lineDescription: 'Móricz Zs. körtér M | Hűvösvölgy',
+              vehicleType: 'TRAM',
+              destination: 'Hűvösvölgy',
+              departureMins: 13
+            }
+          ]
         }
-      ]
+      }
     };
   }
 

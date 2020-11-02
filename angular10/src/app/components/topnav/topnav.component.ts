8ce5b686fc4177ecb63d134498da0b90968b82d1
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2020-11-02 12:27:54 +0100

    Navbar collapse

--- angular10/src/app/components/topnav/topnav.component.ts
+++ angular10/src/app/components/topnav/topnav.component.ts
@@ -9,6 +9,10 @@ export class TopnavComponent implements OnInit {
 
   constructor() { }
 
+  collapseNav(): void {
+    document.getElementById('navbarToggler').click();
+  }
+
   ngOnInit(): void {
   }
 
